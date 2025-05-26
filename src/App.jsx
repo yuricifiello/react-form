bfdd864076da1520476ed9ee0c1e91873ab553ae
yuricifiello <yuricifiello@gmail.com>
2025-05-26 18:25:39 +0200

    exercise complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
-import Header from "./components/layouts/Header";
-import Main from "./components/layouts/Main";
-import Footer from "./components/layouts/Footer";
+import React from "react";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
+  const [articoli, setArticoli] = useState([
+    "Harry Potter e il Calice di Fuoco",
+    "Il Signore Degli Anelli",
+    "The Conjuring",
+  ]);
+
+  const [nuovoTitolo, setNuovoTitolo] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setArticoli([...articoli, nuovoTitolo]);
+
+    setNuovoTitolo("");
+  };
+
   return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
+    <div>
+      <h1>Articoli del Blog</h1>
+
+      <ul>
+        {articoli.map((titolo, index) => (
+          <li key={index}>{titolo}</li>
+        ))}
+      </ul>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Inserisci un nuovo titolo"
+          value={nuovoTitolo}
+          onChange={(e) => setNuovoTitolo(e.target.value)}
+        />
+        <button type="submit">Aggiungi Articolo</button>
+      </form>
+    </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import * as bootstrap from "bootstrap";
 import "./assets/css/index.css";
 import App from "./App.jsx";
 
